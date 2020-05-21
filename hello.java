1be0821ae32899a4329daab7dfd4f8165a9d657a
tuhindewan <tuhinsshadow@gmail.com>
2020-05-21 22:02:46 +0600

    Create amin Hello class and an inner class named greeting

---
+++ hello.java
@@ -0,0 +1,12 @@
+public class Hello { 
+    
+    public static void main(String[] args) { 
+        System.out.println("VS code Awesome....."); 
+        greeting("Tuhin") 
+    }
+
+    public static void greeting(String name) { 
+        System.out.println("Wlcome" + name); 
+    }
+
+}
