3dca0e828f893642ae84c9ec0248f1b821e18cf9
tuhindewan <tuhinsshadow@gmail.com>
2020-05-27 19:03:17 +0600

    Add Burger

--- app.py
+++ app.py
@@ -3,5 +3,6 @@ hungry = input("Are you hungry? ")
 if hungry == 'yes':
     print ("Then eat pasta")
     print ("Then Sleep")
+    print ("Then Burger")
 else:
     print ("Then start learning Git")
