33781445611bb935726234e967cd7aeaa32b4235
tuhindewan <tuhinsshadow@gmail.com>
2020-05-27 19:07:53 +0600

    Revert "Add Burger"
    This reverts commit 3dca0e828f893642ae84c9ec0248f1b821e18cf9.

--- app.py
+++ app.py
@@ -3,6 +3,5 @@ hungry = input("Are you hungry? ")
 if hungry == 'yes':
     print ("Then eat pasta")
     print ("Then Sleep")
-    print ("Then Burger")
 else:
     print ("Then start learning Git")
